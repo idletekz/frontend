d40e325e54220c97134f673716fe27f9f205cd27
idletekz <idletekz@gmail.com>
2019-07-01 13:10:43 -0400

    'react-csv'

--- src/components/ExportData.js
+++ src/components/ExportData.js
@@ -1,41 +1,19 @@
 import React from 'react'
+import { CSVLink } from 'react-csv'
+import Button from 'react-bootstrap/Button'
+
 
 function ExportData(props) {
 
-let obj = [
-    [
-      "New Visitor",
-      "(not set)",
-      "(not set)",
-      "0"
-    ],
-    [
-      "New Visitor",
-      "(not set)",
-      "(not set)",
-      "mobile"
-    ],
-    [
-      "New Visitor",
-      "(not set)",
-      "(not set)",
-      "mobile"
-    ],
-    [
-      "New Visitor",
-      "(not set)",
-      "(not set)",
-      "mobile",
-    ]
-  ]
-  let csv = obj.join('\n')
-	const data = "data:text/plain;charset=UTF-8," + csv;
-
   return (
-		<a class="btn btn-info" role="button" href={data}
-   		download="data.csv">
-  		Download
-		</a>
+  <CSVLink
+    data={props.value}
+    filename={"my-file.csv"}
+    className="btn btn-primary"
+    target="_blank"
+  >
+    Download me
+  </CSVLink>
   );
 }	
 
